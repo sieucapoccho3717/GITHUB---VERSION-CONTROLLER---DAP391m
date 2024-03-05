9c04bde04c70dbee401f1a9012c8652ee5231a02
sieucapoccho3717 <lxlxlx2004@gmail.com>
2024-03-05 10:19:41 +0700

    update 2 func
    isEvenNumber
    isSquareNumber

--- github_version.py
+++ github_version.py
@@ -1,4 +1,17 @@
 print('hello world')
-print('hello world')
-print('hello world')
-print('hello world')
+
+def isEvenNumber(n: int):
+    pass
+
+def isSquareNumber(n: int):
+    pass
+
+if __name__ == "__main__":
+    n = int(input())
+    cnt = 0
+
+    for i in range(1, n + 1):
+        if (isEvenNumber(i) and isSquareNumber(i)):
+            cnt += 1
+    
+    print(cnt)
